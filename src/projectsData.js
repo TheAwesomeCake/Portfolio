74d147a7a738bd1303210ec05c4eef1cc4b88f63
Lucas Barbosa dos Santos <79611129+TheAwesomeCake@users.noreply.github.com>
2025-10-09 17:26:59 -0300

    Add project 'Portifolio'
    Initial commit of the portfoliorepository.
    
    This commit includes the full source code from the project

---
+++ src/projectsData.js
@@ -0,0 +1,78 @@
+import co1 from './assets/conecta_ongs-1.png';
+import co2 from './assets/conecta_ongs-2.png';
+import co3 from './assets/conecta_ongs-3.png';
+import co4 from './assets/conecta_ongs-4.png';
+
+import wb1 from './assets/WB-1.png';
+import wb2 from './assets/WB-2.png';
+import wb3 from './assets/WB-3.png';
+
+import nco1 from './assets/CO-1.png';
+import nco2 from './assets/CO-2.png';
+import nco3 from './assets/CO-3.png';
+import nco4 from './assets/CO-4.png';
+import nco5 from './assets/CO-5.png';
+
+import gv1 from './assets/GV-1.png';
+import gv2 from './assets/GV-2.png';
+import gv3 from './assets/GV-3.png';
+import gv4 from './assets/GV-4.png';
+import gv5 from './assets/GV-5.png';
+import gv6 from './assets/GV-6.png';
+
+export const projects = [
+  {
+    id: 1,
+    title: 'Conecta ONGS - Frontend',
+    shortDescription: 'Frontend de uma plataforma para conectar ONGs a voluntários.',
+    longDescription: 'Projeto Interdisciplinar do segundo semestre da faculdade de Desenvolvimento de Software Multiplataforma.',
+    images: [
+      co1,
+      co2,
+      co3,
+      co4
+    ],
+    githubLink: 'https://github.com/TheAwesomeCake/Conecta-ONGS-Frontend'
+  },
+  {
+    id: 2,
+    title: 'Dashboard de Assessoria',
+    shortDescription: 'Backend robusto em spring, em que trabalhei no frontend e sua interação com o backend.',
+    longDescription: 'Projeto Interdisciplinar do terceiro semestre da faculdade de Desenvolvimento de Software Multiplataforma.',
+    images: [
+      wb1,
+      wb2,
+      wb3
+    ],
+    githubLink: 'https://github.com/TheAwesomeCake/Acessoria-Dashboard'
+  },
+  {
+    id: 3,
+    title: 'Novo Conecta ONGS',
+    shortDescription: 'Projeto o qual trabalhei fullstack, com Node.JS e React, o qual é uma reconstrução do projeto do segundo semestre.',
+    longDescription: 'Projeto Interdisciplinar do quarto semestre da faculdade de Desenvolvimento de Software Multiplataforma inspirado em outro projeto em meu repositório.',
+    images: [
+      nco1,
+      nco2,
+      nco3,
+      nco4,
+      nco5
+    ],
+    githubLink: 'https://github.com/TheAwesomeCake/Novo-Conecta-ONGS'
+  },
+  {
+    id: 4,
+    title: 'Game Vault',
+    shortDescription: 'Projeto o qual trabalhei no Frontend, com React, o qual é uma loja de jogos online.',
+    longDescription: 'Projeto Interdisciplinar do quinto semestre da faculdade de Desenvolvimento de Software Multiplataforma.',
+    images: [
+      gv1,
+      gv2,
+      gv3,
+      gv4,
+      gv5,
+      gv6
+    ],
+    githubLink: 'https://github.com/TheAwesomeCake/Game-Vault'
+  }
+];
