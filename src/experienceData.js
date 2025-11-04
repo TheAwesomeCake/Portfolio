a03d75b18663607c98884524395f4c513255ac01
Lucas Barbosa dos Santos <79611129+TheAwesomeCake@users.noreply.github.com>
2025-11-04 14:54:18 -0300

    New CV

--- src/experienceData.js
+++ src/experienceData.js
@@ -2,7 +2,7 @@ export const experiences = [
     {
       id: 1,
       title: 'Desenvolvedor Back-end',
-      company: 'Faculdade de Tecnologia de Iaquera - FATEC Itaquera',
+      company: 'Faculdade de Tecnologia de Itaquera - FATEC Itaquera',
       period: 'Set 2024 - Dez 2024',
       description: 'Desenvolvimento de APIs RESTful utilizando php e MySQL para gerenciar eventos acadêmicos e cursos de extensão.',
       technologies: ['PHP', 'MySQL', 'Postman']
